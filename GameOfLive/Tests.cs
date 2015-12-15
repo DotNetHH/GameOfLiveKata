4dac0c741c32458f8dbf5a947e43a11c4f45f11b
sven-s <sven@sven-s.de>
2015-12-15 21:23:02 +0100

    Created empty project with NUnit

---
+++ GameOfLive/Tests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace GameOfLive
+{
+
+	public class Tests
+	{
+		[TestCase]
+		public void First_Test_Shoul_Fail()
+		{
+			Assert.IsTrue(false);	
+		}
+
+	}
+}
